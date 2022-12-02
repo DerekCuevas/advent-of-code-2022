141f7d6fe74bdd5ef3392176e10cfa71c2a503bb
Derek Cuevas <dccuevas2@gmail.com>
2022-12-01 20:06:37 -1000

    day two part two

--- src/aoc_2022/two.clj
+++ src/aoc_2022/two.clj
@@ -4,26 +4,44 @@
 
 (def input (h/aoc-input 2022 2))
 
-(defn parse-input-line [s]
-  (let [[opponent self] (s/split s #" ")]
-    [({"A" :rock "B" :paper "C" :scissors} opponent)
-     ({"X" :rock "Y" :paper "Z" :scissors} self)]))
-
 (def choice->points {:rock 1 :paper 2 :scissors 3})
 
 (def choice->beats {:paper :rock, :scissors :paper, :rock :scissors})
 
-(defn rock-paper-scissors [opponent self]
+(def choice->looses (clojure.set/map-invert choice->beats))
+
+(defn- rock-paper-scissors [opponent self]
   (cond
     (identical? opponent self) 3
     (identical? opponent (get choice->beats self)) 6
     :else 0))
 
-(defn part-one [input]
+(defn- play-rounds [parse input]
   (->> input
-       (map parse-input-line)
+       (map parse)
        (map #(+ (choice->points (second %))
                 (apply rock-paper-scissors %)))
        (reduce +)))
 
+(defn- parse-input-line-part-one [s]
+  (let [[opponent self] (s/split s #" ")]
+    [({"A" :rock "B" :paper "C" :scissors} opponent)
+     ({"X" :rock "Y" :paper "Z" :scissors} self)]))
+
+(defn part-one [input]
+  (play-rounds parse-input-line-part-one input))
+
 ;; (part-one input)
+
+(defn- parse-input-line-part-two [s]
+  (let [[opponent self] (s/split s #" ")
+        opponent-choice ({"A" :rock "B" :paper "C" :scissors} opponent)]
+    [opponent-choice
+     ({"X" (choice->beats opponent-choice)
+       "Y" opponent-choice
+       "Z" (choice->looses opponent-choice)} self)]))
+
+(defn part-two [input]
+  (play-rounds parse-input-line-part-two input))
+
+;; (part-two input)
