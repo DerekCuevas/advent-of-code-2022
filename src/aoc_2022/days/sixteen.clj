ab5656c9610c237ae81b50bcb2a3ddbd777dfbcf
Derek Cuevas <dccuevas2@gmail.com>
2022-12-27 12:37:46 -0800

    day sixteen part one wip

--- src/aoc_2022/days/sixteen.clj
+++ src/aoc_2022/days/sixteen.clj
@@ -15,10 +15,29 @@
        (map #(vector (:valve %) %))
        (into {})))
 
-(comment (parse-input (h/aoc-example-input 2022 16)))
+(def initial-budget 30)
+(def open-time 1)
+(def travel-time 1)
+
+(def max-flow-possible
+  (memoize
+   (fn [lookup budget valve]
+     (let [{:keys [rate connections]} (get lookup valve)]
+       (cond
+         (= budget 0) 0
+         (= budget open-time) rate
+         :else
+         (let [cost (if (zero? rate) travel-time (+ open-time travel-time))
+               remaining-budget (- budget cost)
+               connections-max-flow-possible (map #(max-flow-possible lookup remaining-budget %) connections)]
+           (+ (* rate (- budget open-time)) (apply max connections-max-flow-possible))))))))
 
 (defn part-one [input] false)
 
 (defn part-two [input] false)
+
+(def example-lookup (parse-input (h/aoc-example-input 2022 16)))
+
+(max-flow-possible example-lookup initial-budget "AA")
 
 (comment (h/aoc [2022 16] part-one part-two))
