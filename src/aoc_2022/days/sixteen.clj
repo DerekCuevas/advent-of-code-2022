1707feaa3986155c543ae7331d1d292ea0caed75
Derek Cuevas <dccuevas2@gmail.com>
2022-12-28 08:38:00 -0800

    day sixteen part one cleanup

--- src/aoc_2022/days/sixteen.clj
+++ src/aoc_2022/days/sixteen.clj
@@ -20,18 +20,21 @@
 
 (def max-flow
   (memoize
-   (fn [network valve open-valves budget]
-     (let [{:keys [rate connections]} (get network valve)]
-       (if (<= budget 0)
-         {:flow 0 :open-valves open-valves}
-         (let [[time-cost valve-flow next-open-valves]
-               (if (or (contains? open-valves valve) (zero? rate))
-                 [travel-time 0 open-valves]
-                 [(+ open-time travel-time) (* rate (- budget open-time)) (conj open-valves valve)])]
-           (as-> connections $
-             (map #(max-flow network % next-open-valves (- budget time-cost)) $)
-             (apply (partial max-key :flow) $)
-             (update $ :flow #(+ valve-flow %)))))))))
+   (fn
+     ([network valve budget]
+      (max-flow network valve budget #{}))
+     ([network valve budget open-valves]
+      (let [{:keys [rate connections]} (get network valve)]
+        (if (< budget open-time)
+          {:flow 0 :open open-valves}
+          (let [[time-cost valve-flow next-open-valves]
+                (if (or (contains? open-valves valve) (zero? rate))
+                  [travel-time 0 open-valves]
+                  [(+ open-time travel-time) (* rate (- budget open-time)) (conj open-valves valve)])]
+            (as-> connections $
+              (map #(max-flow network % (- budget time-cost) next-open-valves) $)
+              (apply (partial max-key :flow) $)
+              (update $ :flow #(+ valve-flow %))))))))))
 
 (def initial-budget 30)
 (def initial-valve "AA")
@@ -39,15 +42,13 @@
 (defn part-one [input]
   (-> input
       (parse-input)
-      (max-flow initial-valve #{} initial-budget)
+      (max-flow initial-valve initial-budget)
       (get :flow)))
 
 (defn part-two [input] false)
 
 (def example-network (parse-input (h/aoc-example-input 2022 16)))
 
-(comment (max-flow example-network "AA" #{} initial-budget))
-
-(comment (max-flow (parse-input (h/aoc-input 2022 16)) "AA" #{} initial-budget))
+(comment (max-flow example-network "AA" initial-budget))
 
 (comment (h/aoc [2022 16] part-one part-two))
